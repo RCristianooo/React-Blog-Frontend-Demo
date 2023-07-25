49a780147dcfade93def3045b7707a851a3f996a
Cristian <r.cristiano.ai@gmail.com>
2023-07-25 16:27:36 -0400

    Finished up with the frontend of the blog project; added many many more details in components and pages files, including NavBar, HomePage, NewPostPage, and PostPage.

--- src/pages/NewPostPage.js
+++ src/pages/NewPostPage.js
@@ -1,15 +1,59 @@
 import React, {useState} from 'react'
 import axios from 'axios'
-import { Navigate, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { Form, Button, Container } from 'react-bootstrap'
 
 
 const NewPostPage = () => {
     
-    
+    const [post, setPost] = useState({
+        title: '',
+        author: '',
+        image: '',
+        content: ''
+    })
+
+    const navigate = useNavigate()
+
+    const handleChange = e => {
+        setPost({...post, [e.target.name]: e.target.value })
+    }
+
     const handleSubmit = async e => {
         e.preventDefault()
         await axios.post('http://localhost:5500/posts', post)
         navigate('/')
     }
+
+    return(
+        <Container className='mt-4'>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group>
+                    <Form.Label>Title</Form.Label>
+                    <Form.Control type='text' name='title' placeholder='Title' onChange={handleChange} required/>
+                </Form.Group>
+
+                <Form.Group>
+                    <Form.Label>Author</Form.Label>
+                    <Form.Control type='text' name='author' placeholder='Author' onChange={handleChange} required/>
+                </Form.Group>
+
+                <Form.Group>
+                    <Form.Label>Image URL</Form.Label>
+                    <Form.Control type='text' name='image' placeholder='Image URL' onChange={handleChange} required/>
+                </Form.Group>
+
+                <Form.Group>
+                    <Form.Label>Content</Form.Label>
+                    <Form.Control as='textarea' rows={5} name='content' placeholder='Content' onChange={handleChange} required/>
+                </Form.Group>
+
+                <Button variant='primary' type='submit'>
+                    Create
+                </Button>
+            </Form>
+        </Container>
+    )
 }
+
+export default NewPostPage;
